6917e330267c7b62369c4214aed9ce4312cffcc9
sushil <sushil@evenrank.com>
2018-01-02 21:49:22 +0530

    styles setup

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,4 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles/global.scss" lang="scss"></style>
